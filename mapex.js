70232f17e7d781803d2471b0eb8028c9be01f59b
sandoqarahma <rahma.san.m@gmail.com>
2019-08-01 10:46:28 +0300

    1

---
+++ mapex.js
@@ -0,0 +1,44 @@
+
+
+* Exercise 1: 
+*
+function doubleValues(arr){
+	var theNewArray = arr.forEach(x => console.log(x*2));
+}
+
+
+
+
+
+
+* 
+Exercise 2: 
+*
+
+function onlylopEvenValues(arr){
+	var narr=[];
+}
+	arr.forEach(num=>{if(num%2==0){
+		narr.push(num);
+
+	}
+console.log(narr);
+
+
+
+
+
+
+ var filtered =["abcde","abc","a","abcd"]
+ .filter(a=>a.lenght>=3);
+ console.log(filtered);
+
+
+ 
+
+
+
+         
+
+
+
